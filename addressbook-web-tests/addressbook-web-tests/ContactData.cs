90428c037fb6b80b3711a51d59d1e47178f43cc1
Yana Ivashevskaya <Yana.Ivashevskaya@lanit-tercom.com>
2020-04-04 16:21:31 +0300

    ex4

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAdressbookTests
+{
+    class ContactData
+    {
+        private string fname;
+        private string sname = "";
+        private string lname = "";
+
+        public ContactData(string fname)
+        {
+            this.fname = fname;
+        }
+
+        public string Fname
+        {
+            get
+            {
+                return fname;
+            }
+            set
+            {
+                fname = value;
+            }
+        }
+
+        public string Sname
+        {
+            get
+            {
+                return sname;
+            }
+            set
+            {
+                sname = value;
+            }
+        }
+
+        public string Lname
+        {
+            get
+            {
+                return lname;
+            }
+            set
+            {
+                lname = value;
+            }
+        }
+    }
+}
