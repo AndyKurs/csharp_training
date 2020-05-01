b3cc6b37e3f2defb54551098c17076ad08818f5e
Yana Ivashevskaya <Yana.Ivashevskaya@lanit-tercom.com>
2020-05-01 14:06:54 +0300

    EX11

--- addressbook-web-tests/addressbook-web-tests/appmanager/ContactHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ContactHelper.cs
@@ -138,8 +138,10 @@ namespace WebAdressbookTests
 
         public ContactHelper SelectContact()
         {
-            driver.FindElement(By.XPath("//table[@id='maintable']/tbody/tr[2]/td/input")).Click();     //By.Id("5")).Click(); //???
-           
+            driver.FindElement(By.XPath("//table[@id='maintable']/tbody/tr[2]/td/input")).Click();   ////table[@id='maintable']/tbody/tr[2]/td[8]/a/img  
+           // driver.FindElements(By.Name("entry"))[0]
+           //.FindElements(By.TagName("tg"))[7]       // 8 ?
+           //.FindElement(By.TagName("a")).Click();
             acceptNextAlert = true;
             return this;
         }
@@ -174,5 +176,53 @@ namespace WebAdressbookTests
                 acceptNextAlert = true;
             }
         }
+
+        public ContactData GetContactInformationFromTable(int index)
+        {
+            manager.Navigator.GoToHomePage();
+            IList<IWebElement> cells = driver.FindElements(By.Name("entry"))[index].FindElements(By.TagName("td"));
+            string lastName = cells[1].Text;
+            string firstName = cells[2].Text;
+            string address = cells[3].Text;
+            string allEMails = cells[4].Text;
+            string allPhones = cells[5].Text;
+
+            return new ContactData(firstName, lastName)
+            {
+                Address = address,
+                AllPhones = allPhones,
+                AllEmails = allEMails
+            };
+            //throw new NotImplementedException();
+        }
+
+        public ContactData GetContactInformationFromEditForm(int index)
+        {
+            manager.Navigator.GoToHomePage();
+            SelectContactModification();
+            string firstName = driver.FindElement(By.Name("firstname")).GetAttribute("value");
+            string lastName = driver.FindElement(By.Name("lastname")).GetAttribute("value");
+            string address = driver.FindElement(By.Name("address")).GetAttribute("value");
+            string homePhone = driver.FindElement(By.Name("home")).GetAttribute("value");
+            string mobilePhone = driver.FindElement(By.Name("mobile")).GetAttribute("value");
+            string workPhone = driver.FindElement(By.Name("work")).GetAttribute("value");
+            string eMail = driver.FindElement(By.Name("email")).GetAttribute("value");
+            string eMail2 = driver.FindElement(By.Name("email2")).GetAttribute("value");
+            string eMail3 = driver.FindElement(By.Name("email3")).GetAttribute("value");
+
+            return new ContactData(firstName, lastName)
+            {
+                Address = address,
+                HomePhone = homePhone,
+                MobilePhone = mobilePhone,
+                WorkPhone = workPhone,
+                Email = eMail,
+                Email2 = eMail2,
+                Email3 = eMail3
+
+            };
+            //throw new NotImplementedException();
+        }
+
     }
 }
--- addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
@@ -9,6 +9,9 @@ namespace WebAdressbookTests
 {
     public class ContactData : IEquatable<ContactData>, IComparable<ContactData>
     {
+        private string allPhones;
+        private string allEmails;
+
         //private string fname;
         //private string sname = "";
         //private string lname = "";
@@ -59,13 +62,80 @@ namespace WebAdressbookTests
 
         public string Fname { get; set; }
         
-
         public string Sname { get; set; }
-        
 
         public string Lname { get; set; }
 
         public string Id { get; set; }
 
+        public string Address { get; set; }
+
+        public string HomePhone { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        public string WorkPhone { get; set; }
+
+        public string AllPhones
+        {
+            get
+            {
+                if (allPhones != null)
+                {
+                    return allPhones;
+                }
+                else
+                {
+                    return (CleanUp(HomePhone) + CleanUp(MobilePhone) + CleanUp(WorkPhone)).Trim();
+                }
+            }
+            set
+            {
+                allPhones = value;
+            }
+        }
+
+        public string AllEmails
+        {
+            get
+            {
+                if (allEmails != null)
+                {
+                    return allEmails;
+                }
+                else
+                {
+                    return (CleanUpM(Email) + CleanUpM(Email2) + CleanUpM(Email3)).Trim();
+                }
+            }
+            set
+            {
+                allEmails = value;
+            }
+        }
+
+        private string CleanUp(string phone)
+        {
+            if (phone == null || phone == "")
+            {
+                return "";
+            }
+            return phone.Replace(" ","").Replace("-", "").Replace("(", "").Replace(")", "") + "\r\n";
+        }
+
+        private string CleanUpM(string email)
+        {
+            if (email == null || email == "")
+            {
+                return "";
+            }
+            return email.Replace(" ", "") + "\r\n";
+        }
+
+        public string Email { get; set; }
+
+        public string Email2 { get; set; }
+
+        public string Email3 { get; set; }
     }
 }
